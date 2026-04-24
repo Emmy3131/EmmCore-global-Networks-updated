75c1c5bf4035d616bc54d1117ac3943ba5c403d7
Emmy3131 <devinechild3003@gmail.com>
2026-04-24 18:34:52 +0100

    wip

--- controller/authController.js
+++ controller/authController.js
@@ -49,6 +49,7 @@ exports.signup = catchAsync(async (req, res, next) => {
 });
 
 exports.login = catchAsync(async (req, res, next) => {
+  
   //1) Check if email and password exist
   const { email, password } = req.body;
   if (!email || !password) {
