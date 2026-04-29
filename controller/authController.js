243cc6cf6fb7ff2ef60e6b5465204e533c9ee7f9
Emmy3131 <devinechild3003@gmail.com>
2026-04-29 16:29:25 +0100

    wip

--- controller/authController.js
+++ controller/authController.js
@@ -87,8 +87,8 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   await user.save({ validateBeforeSave: false });
 
   // ✅ 3) CREATE RESET LINK
-  const resetURL =
-    `${req.protocol}://${req.get("host")}/resetPassword/${resetToken}`;
+ const resetURL =
+`https://emm-core-global-networks-updated.vercel.app/reset-password/${resetToken}`;
 
   try {
 
