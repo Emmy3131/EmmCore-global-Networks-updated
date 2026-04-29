351f25bbf2a5c15b5067a5799a04fa3af9c45a77
Emmy3131 <devinechild3003@gmail.com>
2026-04-29 16:05:30 +0100

    wip

--- utils/email.js
+++ utils/email.js
@@ -10,9 +10,7 @@ module.exports = class Email {
   // ================= TRANSPORT =================
   createTransport() {
     return nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
-      secure: false,
+      service: "gmail",
       auth: {
         user: process.env.EMAIL_USERNAME,
         pass: process.env.EMAIL_PASSWORD,
